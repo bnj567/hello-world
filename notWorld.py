0be051715756d44b6b5af7452a554dbe4be71cfb
bnj567 <fr0w4w4ym41l4u000001x@outlook.com>
2018-06-04 14:57:26 +0930

    Extended and paused at end
    Extended and paused at end to make it look nice, Really just testing out github desktop.

--- notWorld.py
+++ notWorld.py
@@ -1,4 +1,8 @@
 def dab():
-  print("Hello World")
+  print("Hello, World!")
+  print("What is your name?")
+  name = raw_input("Name: ")
+  print("Hello " + name + "!")
+  nothing = raw_input("Press enter to end...")
 
 dab()
